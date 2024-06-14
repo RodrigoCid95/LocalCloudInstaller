cb85e81e17046ff4024fe0251a8f54d34671df5f
Rodrigo Cid <rodrigocid95@gmail.com>
2024-06-14 08:04:45 +0000

    Update server

--- local-cloud/public/js/p-78bffe29.js
+++
@@ -1,5 +0,0 @@
-import{w as e}from"./p-3647f076.js";
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const n=()=>{if(e!==undefined){return e.Capacitor}return undefined};export{n as g};
-//# sourceMappingURL=p-78bffe29.js.map
