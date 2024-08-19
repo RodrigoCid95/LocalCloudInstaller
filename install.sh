9ddecd8093c1f00b3c409ed8fca62d5fd40bf5f1
Rodrigo Cid <rodrigocid95@gmail.com>
2024-08-18 20:32:39 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,6 +31,46 @@ else
     apt-get install -y nodejs
 fi
 
+if ! dpkg -l | grep -q "^ii mongodb-org"; then
+    echo "Instalando MongoDB ..."
+    apt install -y gnupg curl
+    curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
+    echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+    apt update
+    apt install -y mongodb-org
+    echo "mongodb-org hold" | dpkg --set-selections
+    echo "mongodb-org-database hold" | dpkg --set-selections
+    echo "mongodb-org-server hold" | dpkg --set-selections
+    echo "mongodb-mongosh hold" | dpkg --set-selections
+    echo "mongodb-org-mongos hold" | dpkg --set-selections
+    echo "mongodb-org-tools hold" | dpkg --set-selections
+    systemctl enable mongod
+    systemctl daemon-reload
+    systemctl start mongod
+else
+    echo "MongoDB ya está instalado."
+fi
+
+if ! command -v nginx >/dev/null 2>&1; then
+    echo "instalando Nginx ..."
+    sudo apt update
+    sudo apt install -y nginx
+    if [ $? -eq 0 ]; then
+        echo "nginx se ha instalado correctamente."
+        mkdir /etc/nginx/ssl_certificate
+        openssl genrsa -out /etc/nginx/ssl_certificate/key.pem
+        openssl req -new -key /etc/nginx/ssl_certificate/key.pem -out /etc/nginx/ssl_certificate/csr.pem -config ./openssl.cnf
+        openssl x509 -req -days 9999 -in /etc/nginx/ssl_certificate/csr.pem -signkey /etc/nginx/ssl_certificate/key.pem -out /etc/nginx/ssl_certificate/cert.pem
+        systemctl enable nginx
+        systemctl stop nginx
+    else
+        echo "Hubo un problema al instalar nginx."
+        exit 1
+    fi
+else
+    systemctl stop nginx
+fi
+
 if ! dpkg -l | grep -qw samba; then
     echo "Instalando Samba ..."
     apt-get update
@@ -60,6 +100,7 @@ fi
 chmod +x ./install
 ./install
 systemctl start local-cloud
+systemctl start nginx
 IP=$(hostname -I)
 IP=$(echo "$IP" | tr -d '[[:space:]]')
 echo "Puedes iniciar sesión desde http://$IP:3001"
