9415ec50199f3fc3f58a5db993a18db5acfe39e4
Rodrigo Cid <rodrigocid@gmail.com>
2024-11-15 00:32:55 -0600

    Update installer

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ]; then
-    echo "Este script debe ejecutarse como root" >&2
-    exit 1
+	echo "Este script debe ejecutarse como root" >&2
+	exit 1
 fi
 
 if systemctl list-units --type=service --all | grep -q "local-cloud"; then
-    echo "LocalCloud ya está instalando."
+	echo "LocalCloud ya está instalando."
 fi
 
 if ! command -v nginx >/dev/null 2>&1; then
@@ -29,20 +29,36 @@ else
 fi
 
 if ! dpkg -l | grep -qw samba; then
-    echo "Instalando Samba ..."
-    apt-get update
-    apt-get install -y samba
-    if [ $? -eq 0 ]; then
-        echo "Samba ha sido instalado exitosamente."
-    else
-        echo "Hubo un problema al instalar Samba."
-        exit 1
-    fi
+	echo "Instalando Samba ..."
+	apt-get update
+	apt-get install -y samba
+	if [ $? -eq 0 ]; then
+		echo "Samba ha sido instalado exitosamente."
+	else
+		echo "Hubo un problema al instalar Samba."
+		exit 1
+	fi
 fi
 
 if ! grep -q "^lc:" /etc/group; then
-    groupadd lc
+	groupadd lc
 fi
+
+cp ./local-cloud /etc
+mkdir /var/local-cloud
+set -eu
+dir="/etc/local-cloud"
+platform=$(uname -ms)
+tgz="$dir/esbuild-0.24.0.tgz"
+case $platform in
+  'Linux arm64' | 'Linux aarch64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/linux-arm64/-/linux-arm64-0.24.0.tgz";;
+  'Linux x86_64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/linux-x64/-/linux-x64-0.24.0.tgz";;
+  *) echo "error: Unsupported platform: $platform"; exit 1
+esac
+tar -xzf "$tgz" -C "$dir" package/bin/esbuild
+mv "$dir/package/bin/esbuild" "$dir"
+rm "$tgz"
+rm -R "$dir/package"
 
 chmod +x ./install
 ./install
